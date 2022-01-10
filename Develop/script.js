f327cd2b1164f471930ec564b2126acd1170c36c
htam <hdfair@gmail.com>
2022-01-10 18:28:10 -0500

    add query about length of password

--- Develop/script.js
+++ Develop/script.js
@@ -9,31 +9,20 @@ function getPasswordLength(valueEntered) {
     promptMessage = valueEntered + " is not from 8-128. " + promptMessage;
   }
 
-  const defaultLength = 8;
-  var length = Number(prompt(promptMessage, defaultLength));
   
-  // If the value is not a number or isn't between 8-128 then re run function
-  if (isNaN(length)) {
-    return defaultLength;
-  } else if (length < 8 || length > 128) {
-    return getPasswordLength(length.toString());
-  } else {
-    return length;
+
+
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
   }
-}
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
